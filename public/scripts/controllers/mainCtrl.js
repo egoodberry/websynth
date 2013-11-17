3956c402b3967a492363559b1b0c73d2e1a3ada4
Evan Goodberry <egoodberry@gmail.com>
2013-11-17 17:04:48 -0500

    Experimenting with arpeggiation.

--- public/scripts/controllers/mainCtrl.js
+++ public/scripts/controllers/mainCtrl.js
@@ -2,16 +2,37 @@
   ns.registerController('MainCtrl', function($scope) {
     $scope.voices = [];
 
+    $scope.arpeggiate = true;
+    $scope.arpeggiationSpeed = 200;
+    $scope.arpeggioSort = true;
+    var frequencies = [];
+
     var keyboard = new QwertyHancock({ id: "keyboard" });
     keyboard.keyDown = function (_, frequency) {
-      play(frequency);
+      if ($scope.arpeggiate) {
+        frequencies.push(frequency);
+        if ($scope.arpeggioSort) {
+          frequencies.sort(numericalSorter);
+        }
+        playArpeggio();
+      }
+      else {
+        play(frequency);
+      }
+    };
+    keyboard.keyUp = function(_, frequency) {
+      for (var i = 0; i < frequencies.length; i++) {
+        if (frequencies[i] === frequency) {
+          frequencies.splice(i, 1);
+        }
+      }
     };
 
     $scope.addVoice = function() {
       $scope.voices.push(new ns.Voice($scope));
     };
 
-    $scope.addVoice();
+    $scope.addVoice(); // default voice
 
     var play = function(frequency) {
       for (var i = 0; i < $scope.voices.length; i++) {
@@ -19,29 +40,29 @@
       }
     };
 
+    var playingArpeggio = false;
+    var playArpeggio = function() {
+      if (playingArpeggio) { return }
+
+      playingArpeggio = true;
+
+      playNext(0);
+
+      playingArpeggio = false;
+    };
+
+    var playNext = function(index) {
+      play(frequencies[index]);
+      if (frequencies.length > 0) {
+        index = (index + 1) % frequencies.length;
+        setTimeout(function() { playNext(index); }, $scope.arpeggiationSpeed);
+      }
+    };
+
+    var numericalSorter = function(x, y) {
+      return x - y;
+    };
+
     ns.Midi.initialize();
-
-    // var arpeggiator;
-    // $scope.arpeggiate = function() {
-    // if (oscillator) {
-    // arpeggiator = new ns.arpeggiator(oscillator);
-    // arpeggiator.start();
-    // }
-    // };
-
-    // var buildWave = function(voice) {
-      // var real = new Float32Array(4096);
-      // var imag = new Float32Array(4096);
-
-      // var a1 = 0.0;
-      // var b1 = 1.0;
-
-      // var shift = 2 * Math.PI * 0.5;
-      // real[1] = a1 * Math.cos(shift) - b1 * Math.sin(shift);
-      // imag[1] = a1 * Math.sin(shift) + b1 * Math.cos(shift);
-      // // imag[1] = 1.0;
-
-      // ns.audioContext.createPeriodicWave(real, imag);
-    // };
   });
 })(App);
